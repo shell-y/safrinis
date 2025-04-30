f629c4ea254edbc8ba6997ed2dd90736c25dd162
gustavosptech <gustavo.barbosa@sptech.school>
2025-04-30 18:29:15 -0300

    criação da div loading

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -15,19 +15,13 @@ function limparSessao() {
 
 // carregamento (loading)
 function aguardar() {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "flex";
+    var divAguardar = document.getElementById("loading");
+    divAguardar.style.display = 'block'
 }
 
 function finalizarAguardar(texto) {
     var divAguardar = document.getElementById("div_aguardar");
     divAguardar.style.display = "none";
-
-    var divErrosLogin = document.getElementById("div_erros_login");
-    if (texto) {
-        divErrosLogin.style.display = "flex";
-        divErrosLogin.innerHTML = texto;
-    }
 }
 
 function limparLineup(){
